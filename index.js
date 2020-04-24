d7ab30564460edacc95a99892d123afe5af5dbff
Trần Tuấn Anh <47843163+zzNuAzz@users.noreply.github.com>
2020-04-24 12:21:48 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rita-Ryuunosuke Link</title>
+</head>
+<body>
+    <a href="https://www.facebook.com/SoNguyenTo216"> My facebook profile </a>
+</body>
+</html>
